4af0d69d5e943f3f1140508625e4b2bf2a926a45
rassvyet <rassvyeta@gmail.com>
2025-08-08 12:10:55 -0400

    copying cammy into here so it goes public

---
+++ MyScript.js
@@ -0,0 +1,9 @@
+// this is how you do an inline comment
+
+/* this is a
+multi-lined comment*/ 
+
+
+a = 6;
+console.log(a)
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# just a stupid demo, to showcase my stupid progress!
+
+
+  ## subheader
+
+hello 
